648b3d105d6fae93d4c64f4eea1ac850dcd3e54d
Gustavo Almeida <gustavo.s.p.almeida@gmail.com>
2023-11-06 11:33:15 -0300

    feat: Updated pessoas.controller.listAll to return pesProfissao

--- node-express-prisma-backend/src/controllers/pessoas.controller.ts
+++ node-express-prisma-backend/src/controllers/pessoas.controller.ts
@@ -3,8 +3,21 @@ import { prisma } from "../services/prisma";
 
 export const pessoasController = {
     async listAll(req: Request, res: Response) {
-        const pessoas = await prisma.pessoa.findMany()
+        const pessoas = await prisma.pessoa.findMany({
+            include: {
+                pesProfissao: {
+                    select: {
+                        profNome: true
+                    }
+                }
+            }
+        })
 
-        return res.json(pessoas)
+        const parsedPessoas = pessoas.map(pessoa => ({
+            ...pessoa,
+            pesProfissao: pessoa.pesProfissao?.profNome || null,
+        }));
+        
+        return res.json(parsedPessoas)
     }
 }
